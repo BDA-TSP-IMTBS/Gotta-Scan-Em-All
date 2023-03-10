11b5994e6180ddc4c39da7cbbd0f0ba27d7ce5bc
Artamis <artamis@hackademint.org>
2023-03-10 17:52:21 +0100

    Add QRCode generator

---
+++ generateQRCodes.py
@@ -0,0 +1,23 @@
+import json
+import random, string
+import pyqrcode
+from pyqrcode import QRCode
+
+# This program generates QR codes from slugs
+
+DEFAULT_URL = "https://apero-bda.hackademint.org/"
+
+with open("./items.json", "r") as f:
+  data = json.load(f)
+
+for i in range(len(data['items'])):
+  dest = DEFAULT_URL + data['items'][i]['slug']
+  myQR = QRCode(dest)
+  myQR.png('qrcode'+str(i+1)+'.png', scale=8)
+  
+dest = DEFAULT_URL
+myQR = QRCode(dest)
+myQR.png('qrcodeBASE.png', scale=8)
+    
+
+
